1fc5801c5cfd1e9b8971cdbb7f0ff9a0f558f3e6
ash07-1 <155059989+ash07-1@users.noreply.github.com>
2024-01-19 01:50:51 +0530

    Add files via upload

---
+++ InternProject-II/app.js
@@ -0,0 +1,6 @@
+let signUp = document.getElementsByClassName(".login");
+
+signUp.addEventListener("click",function(){
+    window.alert("Subimtted");   
+    document.getElementById("userForm").reset();
+});
